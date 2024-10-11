ce6ad94d681a2e567f632bd4e29369f402131566
DAMBA Aly <12201332@crit-xubuntu>
2024-10-11 11:53:46 +0200

    ajout if __name__

--- test_fail.py
+++ test_fail.py
@@ -10,3 +10,10 @@ class TestFactorielle(unittest.TestCase):
 
     def test_factorielle_de_5(self):
             self.assertEqual(factorielle(5), 120)
+
+
+
+
+
+if __name__=="__main__":
+    unittest.main()
