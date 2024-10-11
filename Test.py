657c5f21a10da65f24a33dd7d29a398eabbe49a1
DAMBA Aly <12201332@crit-xubuntu>
2024-10-11 12:50:49 +0200

    correction de la classe fizzbuzz

--- Test.py
+++ Test.py
@@ -1,7 +1,23 @@
 class FizzBuzz:
 
-    def __init__(self, nom) -> None:
-        self.nom = nom
+    #def __init__(self, nom) -> None:
+        #self.nom = nom
     
-    def factorielle(self):
-        return (120)
+    #def factorielle(self):
+        #return (120)
+
+
+    def __init__(self, start=1, end=100):
+        self.start = start
+        self.end = end
+
+    def affiche(self):
+        for i in range(self.start, self.end + 1):
+            if i % 15 == 0:
+                print("Frisbee")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
---
+++ test1.py
@@ -0,0 +1,13 @@
+import unittest
+from Test import FizzBuzz
+
+
+# Créer une instance de FizzBuzz avec des paramètres personnalisés
+fizzbuzz = FizzBuzz(start=1, end=20)
+
+# Appeler la méthode affiche
+fizzbuzz.affiche()
+
+
+#if __name__ == "__main__":
+    #fizzbuzz.affiche()
